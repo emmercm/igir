703b23cb032fbf2958d179ec65a9138867b6501b
Christian Emmer <emmercm@gmail.com>
2022-09-23 15:59:38 -0700

    Feature: thread DAT scanning & parsing (#76)

--- src/modules/datScanner.ts
+++ src/modules/datScanner.ts
@@ -1,6 +1,8 @@
+import async, { AsyncResultCallback } from 'async';
 import xml2js from 'xml2js';
 
 import { Symbols } from '../console/progressBar.js';
+import Constants from '../constants.js';
 import bufferPoly from '../polyfill/bufferPoly.js';
 import File from '../types/files/file.js';
 import DAT from '../types/logiqx/dat.js';
@@ -26,38 +28,46 @@ export default class DATScanner extends Scanner {
     await this.progressBar.setSymbol(Symbols.SEARCHING);
     await this.progressBar.reset(datFilePaths.length);
 
-    const parsedDataFiles = await this.parseDataFiles(datFilePaths);
+    const datFiles = await this.getDatFiles(datFilePaths);
+    await this.progressBar.reset(datFiles.length);
 
     await this.progressBar.logInfo('Deserializing DAT XML to objects');
-    const dats = parsedDataFiles
-      .filter((xmlObject) => xmlObject)
-      .map((xmlObject) => DAT.fromObject(xmlObject.datafile))
-      .sort((a, b) => a.getNameShort().localeCompare(b.getNameShort()));
+    const dats = await this.parseDatFiles(datFiles);
+
     await this.progressBar.logInfo(dats.map((dat) => `${dat.getName()}: ${dat.getGames().length} games, ${dat.getParents().length} parents parsed`).join('\n'));
     return dats;
   }
 
-  private async parseDataFiles(datFilePaths: string[]): Promise<DataFile[]> {
-    const results: DataFile[] = [];
+  // Scan files on disk for DATs (archives may yield more than one DAT)
+  private async getDatFiles(datFilePaths: string[]): Promise<File[]> {
+    return (await async.mapLimit(
+      datFilePaths,
+      Constants.DAT_SCANNER_THREADS,
+      async (datFilePath: string, callback: AsyncResultCallback<File[], Error>) => {
+        await this.progressBar.logDebug(`${datFilePath}: Reading file`);
+        const datFiles = await this.getFilesFromPath(datFilePath);
+        callback(null, datFiles);
+      },
+    )).flatMap((datFiles) => datFiles);
+  }
 
-    // TODO(cemmer): thread DAT parsing
-    /* eslint-disable no-await-in-loop */
-    for (let i = 0; i < datFilePaths.length; i += 1) {
-      const datFilePath = datFilePaths[i];
-      await this.progressBar.logDebug(`${datFilePath}: Reading file`);
-      await this.progressBar.increment();
-
-      const datFiles = await this.getFilesFromPath(datFilePath);
-      for (let j = 0; j < datFiles.length; j += 1) {
-        const datFile = datFiles[j];
+  // Parse each file into a DAT
+  private async parseDatFiles(datFiles: File[]): Promise<DAT[]> {
+    const results = (await async.mapLimit(
+      datFiles,
+      Constants.DAT_SCANNER_THREADS,
+      async (datFile: File, callback: AsyncResultCallback<DAT | undefined, Error>) => {
+        await this.progressBar.increment();
         const xmlObject = await this.parseDatFile(datFile);
         if (xmlObject) {
-          results.push(xmlObject);
+          const dat = DAT.fromObject(xmlObject.datafile);
+          return callback(null, dat);
         }
-      }
-    }
+        return callback(null, undefined);
+      },
+    )).filter((xmlObject) => xmlObject) as DAT[];
 
-    return results;
+    return results.sort((a, b) => a.getNameShort().localeCompare(b.getNameShort()));
   }
 
   private async parseDatFile(datFile: File): Promise<DataFile | undefined> {
