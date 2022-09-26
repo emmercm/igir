e2d87716effa59dddc303f90ae29049a930307fe
Christian Emmer <emmercm@gmail.com>
2022-09-25 18:41:10 -0700

    Remove TODO

--- test/types/files/archive.test.ts
+++ test/types/files/archive.test.ts
@@ -1,7 +1,6 @@
 import ArchiveFactory from '../../../src/types/archives/archiveFactory.js';
 
 describe('getArchiveEntries', () => {
-  // TODO(cemmer): fixture archives with multiple entries
   test.each([
     // fizzbuzz
     ['./test/fixtures/roms/7z/fizzbuzz.7z', 'fizzbuzz.nes', '370517b5'],
@@ -28,5 +27,23 @@ describe('getArchiveEntries', () => {
     const entry = entries[0];
     expect(entry.getEntryPath()).toEqual(expectedEntryPath);
     await expect(entry.getCrc32()).resolves.toEqual(expectedCrc);
+  });
+
+  test.each([
+    ['./test/fixtures/roms/7z/onetwothree.7z', [['1/one.rom', 'f817a89f'], ['2/two.rom', '96170874'], ['3/three.rom', 'ff46c5d8']]],
+    ['./test/fixtures/roms/zip/onetwothree.zip', [['1/one.rom', 'f817a89f'], ['2/two.rom', '96170874'], ['3/three.rom', 'ff46c5d8']]],
+  ])('should enumerate the multi file archive: %s', async (filePath, expectedEntries) => {
+    const archive = ArchiveFactory.archiveFrom(filePath);
+
+    const entries = await archive.getArchiveEntries();
+    expect(entries).toHaveLength(expectedEntries.length);
+
+    /* eslint-disable no-await-in-loop */
+    for (let i = 0; i < entries.length; i += 1) {
+      const entry = entries[i];
+      const expectedEntry = expectedEntries[i];
+      expect(entry.getEntryPath()).toEqual(expectedEntry[0]);
+      await expect(entry.getCrc32()).resolves.toEqual(expectedEntry[1]);
+    }
   });
 });
