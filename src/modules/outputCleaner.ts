b402ee6c87c730fc34456eeef742d6113fdab9ee
Christian Emmer <emmercm@gmail.com>
2022-09-27 18:11:31 -0700

    path.normalize()

--- src/modules/outputCleaner.ts
+++ src/modules/outputCleaner.ts
@@ -35,13 +35,13 @@ export default class OutputCleaner {
 
     const outputDir = this.options.getOutput();
     const outputFilePathsToExclude = writtenFilesToExclude
-      .map((file) => fs.realpathSync(file.getFilePath()));
+      .map((file) => path.normalize(file.getFilePath()));
     console.log('excluding');
     console.log(outputFilePathsToExclude);
 
     // If there is nothing to clean, then don't do anything
     const filesToClean = (await fg(`${outputDir}/**`.replace(/\\/g, '/')))
-      .map((file) => fs.realpathSync(file))
+      .map((file) => path.normalize(file))
       .filter((file) => outputFilePathsToExclude.indexOf(file) === -1);
     console.log('cleaning');
     console.log(filesToClean);
