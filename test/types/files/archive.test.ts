bfddeed8f484ee50ed4b70701bc0d9b591e43cee
Christian Emmer <emmercm@gmail.com>
2022-09-27 19:02:13 -0700

    Reverts

--- test/types/files/archive.test.ts
+++ test/types/files/archive.test.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import ArchiveFactory from '../../../src/types/archives/archiveFactory.js';
 
 describe('getArchiveEntries', () => {
+  // TODO(cemmer): fixture archives with multiple entries
   test.each([
     // fizzbuzz
     ['./test/fixtures/roms/7z/fizzbuzz.7z', 'fizzbuzz.nes', '370517b5'],
