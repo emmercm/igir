4d075e5ac05ce55682824bad27cced341183a008
Christian Emmer <emmercm@gmail.com>
2022-12-03 16:38:26 -0500

    Fix

--- src/modules/argumentsParser.ts
+++ src/modules/argumentsParser.ts
@@ -137,7 +137,7 @@ export default class ArgumentsParser {
         }
 
         const needOutput = ['copy', 'move', 'zip', 'clean'].filter((command) => checkArgv._.indexOf(command) !== -1);
-        if ((!checkArgv.output || !checkArgv.output.length) && needOutput.length) {
+        if (!checkArgv.output && needOutput.length) {
           throw new Error(`Missing required option for commands ${needOutput.join(', ')}: output`);
         }
 
