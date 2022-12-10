748f7f297cd9051fd0c2db1ddd363874f36fd908
Christian Emmer <emmercm@gmail.com>
2022-12-09 20:21:06 -0800

    Fix: allow multiple pending progress bar waiting messages (#214)

--- src/console/progressBarCLI.ts
+++ src/console/progressBarCLI.ts
@@ -21,7 +21,7 @@ export default class ProgressBarCLI extends ProgressBar {
 
   private readonly singleBarFormatted: SingleBarFormatted;
 
-  private waitingMessageTimeout?: NodeJS.Timeout;
+  private waitingMessages: { [key: string]: NodeJS.Timeout } = {};
 
   private constructor(logger: Logger, singleBarFormatted: SingleBarFormatted) {
     super();
@@ -96,9 +96,7 @@ export default class ProgressBarCLI extends ProgressBar {
 
   async reset(total: number): Promise<void> {
     this.singleBarFormatted.getSingleBar().setTotal(total);
-    this.singleBarFormatted.getSingleBar().update(0, {
-      waitingMessage: undefined,
-    } as ProgressBarPayload);
+    this.singleBarFormatted.getSingleBar().update(0);
     return ProgressBarCLI.render();
   }
 
@@ -111,31 +109,43 @@ export default class ProgressBarCLI extends ProgressBar {
 
   /**
    * If progress hasn't been made by some timeout period, then show a waiting message to let the
-   *  user know that there is still something processing. Other functions in this class that update
-   *  progress should reset this message.
+   *  user know that there is still something processing.
    */
-  setWaitingMessage(waitingMessage: string, timeout = 10_000): NodeJS.Timeout {
-    clearTimeout(this.waitingMessageTimeout);
-    this.waitingMessageTimeout = setTimeout(async () => {
+  addWaitingMessage(waitingMessage: string, timeout = 10_000): void {
+    // If waitingMessage has a timeout, clear it and delete it
+    if (this.waitingMessages[waitingMessage]) {
+      clearTimeout(this.waitingMessages[waitingMessage]);
+      delete this.waitingMessages[waitingMessage];
+    }
+    // Set a timer to re-render with this waiting message
+    this.waitingMessages[waitingMessage] = setTimeout(async () => {
       this.singleBarFormatted.getSingleBar().update({
         waitingMessage,
       } as ProgressBarPayload);
       await ProgressBarCLI.render(true);
     }, timeout);
-    return this.waitingMessageTimeout;
+  }
+
+  async removeWaitingMessage(waitingMessage: string): Promise<void> {
+    // If waitingMessage has a timeout, clear it and delete it
+    if (this.waitingMessages[waitingMessage]) {
+      clearTimeout(this.waitingMessages[waitingMessage]);
+      delete this.waitingMessages[waitingMessage];
+    }
+    // Re-render using the next waiting message
+    this.singleBarFormatted.getSingleBar().update({
+      waitingMessage: Object.keys(this.waitingMessages).sort()[0],
+    } as ProgressBarPayload);
+    return ProgressBarCLI.render(true);
   }
 
   async increment(): Promise<void> {
-    this.singleBarFormatted.getSingleBar().increment({
-      waitingMessage: undefined,
-    } as ProgressBarPayload);
+    this.singleBarFormatted.getSingleBar().increment();
     return ProgressBarCLI.render();
   }
 
   async update(current: number): Promise<void> {
-    this.singleBarFormatted.getSingleBar().update(current, {
-      waitingMessage: undefined,
-    } as ProgressBarPayload);
+    this.singleBarFormatted.getSingleBar().update(current);
     return ProgressBarCLI.render();
   }
 
@@ -143,15 +153,11 @@ export default class ProgressBarCLI extends ProgressBar {
     await this.setSymbol(Symbols.DONE);
 
     if (this.singleBarFormatted.getSingleBar().getTotal() > 0) {
-      this.singleBarFormatted.getSingleBar().update(
-        this.singleBarFormatted.getSingleBar().getTotal(),
-        { waitingMessage: undefined } as ProgressBarPayload,
-      );
+      this.singleBarFormatted.getSingleBar()
+        .update(this.singleBarFormatted.getSingleBar().getTotal());
     } else {
-      this.singleBarFormatted.getSingleBar().update(
-        this.singleBarFormatted.getSingleBar().getTotal() + 1,
-        { waitingMessage: undefined } as ProgressBarPayload,
-      );
+      this.singleBarFormatted.getSingleBar()
+        .update(this.singleBarFormatted.getSingleBar().getTotal() + 1);
     }
 
     if (finishedMessage) {
