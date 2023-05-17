8fbb89f170f1e20673913306c880b192194edb10
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2023-05-16 17:14:46 -0700

    Increase test coverage

---
+++ test/elasticSemaphore.test.ts
@@ -0,0 +1,17 @@
+import ElasticSemaphore from '../src/elasticSemaphore.js';
+
+describe('runExclusive', () => {
+  test.each([
+    -1,
+    0,
+    1,
+    10,
+    1_000_000,
+  ])('should accept any weight: %s', async (weight) => {
+    const semaphore = new ElasticSemaphore(5);
+
+    const message = 'success!';
+    const result = await semaphore.runExclusive(() => message, weight);
+    expect(result).toEqual(message);
+  });
+});
