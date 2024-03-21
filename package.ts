6515e728be63deab6714b579a59d63ebc42c65b3
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-03-20 21:28:34 -0700

    Package only the needed chdman

--- package.ts
+++ package.ts
@@ -85,6 +85,9 @@ const fileFilter = (filters: FileFilter[]): string[] => {
     // Only include the exact 7zip-bin we need
     { exclude: 'node_modules/{**/,}7zip-bin/**/7z*' },
     { include: path7za },
+    // Only include the exact chdman bin we need
+    { exclude: 'node_modules/{**/,}chdman/bin/*/*/chdman*' },
+    { include: `node_modules/{**/,}chdman/bin/${process.platform}/${process.arch}/chdman*` },
   ]));
   const includeSize = (await Promise.all([...include].map(async (file) => {
     if (await FsPoly.isDirectory(file)) {
