c7bd3f38ef6bc2dae7af14c115f380d1c7744a38
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-04-06 20:18:59 -0700

    Update parent inference as well

--- src/modules/datParentInferrer.ts
+++ src/modules/datParentInferrer.ts
@@ -137,7 +137,7 @@ export default class DATParentInferrer extends Module {
       .replace(/\(M[0-9]+\)/, '') // language
       .replace(/\((CW|CW-R|FW|GW|GW-R|LW|PD|SW|SW-R)\)/i, '') // copyright
       .replace(/\((alpha|beta|preview|pre-release|proto)\)/i, '') // development
-      .replace(/(\[(cr|f|h|m|p|t|tr|o|u|v|b|a|!)( [a-z0-9.+ -]+)?\])+/i, '')
+      .replace(/(\[(cr|f|h|m|p|t|tr|o|u|v|b|a|!)([0-9]+| [^\]]+)?\])+/i, '')
       // ***** Console-specific *****
       // Nintendo - Game Boy
       .replace(/\(SGB Enhanced\)/i, '')
