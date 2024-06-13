e0c40bbb1463f967ae8c0801e3b20f994856feb6
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-06-13 16:36:29 -0700

    Refactor: defer ArchiveFile checksum calculation until after 1G1R applied (#1162)

--- src/types/files/archives/archiveFile.ts
+++ src/types/files/archives/archiveFile.ts
@@ -4,8 +4,11 @@ import Archive from './archive.js';
 export default class ArchiveFile extends File {
   private readonly archive: Archive;
 
-  public constructor(archive: Archive, fileProps: FileProps) {
-    super(fileProps);
+  public constructor(archive: Archive, fileProps: Omit<FileProps, 'filePath'>) {
+    super({
+      ...fileProps,
+      filePath: archive.getFilePath(),
+    });
     this.archive = archive;
   }
 
---
+++ test/modules/candidateArchiveFileHasher.test.ts
@@ -0,0 +1,3 @@
+it('should be tested', () => {
+  // TODO(cemmer)
+});
