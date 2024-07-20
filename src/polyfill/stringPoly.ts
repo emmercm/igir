42092d25a21fbb614add7250e54badaacf265d17
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-20 13:22:12 -0700

    Delete StringPoly

--- src/polyfill/stringPoly.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  replaceInsensitive(input: string, searchValue: string, replaceValue: string): string {
-    // https://stackoverflow.com/a/21257041
-    const idx = input.toLowerCase().indexOf(searchValue.toLowerCase());
-    return idx === -1
-      ? input
-      : input.substring(0, idx) + replaceValue + input.substring(idx + searchValue.length);
-  },
-};
