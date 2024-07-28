f09754f1a6abab126e5084b4de7d2c1518ea5d2a
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-28 14:39:32 -0700

    Use more efficient uncompressed size fetching

--- src/types/files/archives/maxcso/maxcso.ts
+++ src/types/files/archives/maxcso/maxcso.ts
@@ -2,25 +2,19 @@ import path from 'node:path';
 
 import maxcso from 'maxcso';
 
-import { ChecksumBitmask } from '../../fileChecksums.js';
 import Archive from '../archive.js';
 import ArchiveEntry from '../archiveEntry.js';
 
 export default abstract class Maxcso extends Archive {
   async getArchiveEntries(checksumBitmask: number): Promise<ArchiveEntry<Archive>[]> {
     const entryPath = `${path.parse(this.getFilePath()).name}.iso`;
+    const size = (await maxcso.header(this.getFilePath())).uncompressedSize;
     const crc32 = await maxcso.uncompressedCrc32(this.getFilePath());
-
-    // If this file won't need decompressing to calculate other checksums, then save disk thrash
-    // by calculating its uncompressed size in memory
-    const size = !checksumBitmask || checksumBitmask === ChecksumBitmask.CRC32
-      ? await maxcso.uncompressedSize(this.getFilePath())
-      : 0;
 
     return [await ArchiveEntry.entryOf({
       archive: this,
       entryPath,
-      size,
+      size: Number(size),
       crc32,
     }, checksumBitmask)];
   }
