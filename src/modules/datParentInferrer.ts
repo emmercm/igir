d9faa01439c8426606849a9b4c7d0e6f30c3ddbc
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-29 22:07:37 -0700

    Feature: better Dreamcast parent/clone inference

--- src/modules/datParentInferrer.ts
+++ src/modules/datParentInferrer.ts
@@ -60,6 +60,7 @@ export default class DATParentInferrer extends Module {
       // ***** Regions *****
       .replace(new RegExp(`\\(((${Internationalization.REGION_CODES.join('|')})[,+-]? ?)+\\)`, 'i'), '')
       .replace(new RegExp(`\\(((${Internationalization.REGION_NAMES.join('|')})[,+-]? ?)+\\)`, 'i'), '')
+      .replace(/\(Latin America\)/i, '')
       // ***** Languages *****
       .replace(new RegExp(`\\(((${Internationalization.LANGUAGES.join('|')})[,+-]? ?)+\\)`, 'i'), '')
       // ***** Cleanup *****
@@ -73,6 +74,7 @@ export default class DATParentInferrer extends Module {
       .replace(/\(Alt( [a-z0-9. ]*)?\)/i, '')
       .replace(/\([^)]*Collector's Edition\)/i, '')
       .replace(/\(Extra Box\)/i, '')
+      .replace(/ - European Version/i, '')
       .replace(/\(Fukkokuban\)/i, '') // "reprint"
       .replace(/\([^)]*Genteiban\)/i, '') // "limited edition"
       .replace(/\(Limited[^)]+Edition\)/i, '')
@@ -152,8 +154,8 @@ export default class DATParentInferrer extends Module {
       // Nintendo - Super Nintendo Entertainment System
       .replace(/\(NP\)/i, '') // "Nintendo Power"
       // Sega - Dreamcast
-      .replace(/\[[0-9]+S\]/, '') // boxcode
-      .replace(/\[[0-9]+MM?[0-9]+(, [0-9]+MM?[0-9]+)*\]/, '')
+      .replace(/\[([0-9A-Z ]+(, )?)+\]$/, '') // TOSEC boxcode
+      .replace(/\[(compilation|data identical to retail|fixed version|keyboard|limited edition|req\. microphone|scrambled|unscrambled|white label)\]/ig, '') // TOSEC
       .replace(/for Dreamcast/i, '')
       // Sega - Mega Drive / Genesis
       .replace(/\(MP\)/i, '') // "MegaPlay version"
