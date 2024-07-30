567c0d82d29689921e1ad9feb326afc444232300
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-29 20:08:46 -0700

    Debug

--- src/types/expectedError.ts
+++ src/types/expectedError.ts
@@ -1,4 +1,9 @@
 /**
  * An {@link Error} thrown by application code due to expected reasons such as invalid inputs.
  */
-export default class ExpectedError extends Error {}
+export default class ExpectedError extends Error {
+  constructor(message?: string) {
+    super(message);
+    console.log(`ExpectedError: ${message}`);
+  }
+}
