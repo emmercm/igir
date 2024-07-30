1dcadf22c238d256cd3459904d953f906a89247b
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-07-29 20:24:33 -0700

    Revert "Debug"
    This reverts commit 567c0d82d29689921e1ad9feb326afc444232300.

--- src/types/expectedError.ts
+++ src/types/expectedError.ts
@@ -1,9 +1,4 @@
 /**
  * An {@link Error} thrown by application code due to expected reasons such as invalid inputs.
  */
-export default class ExpectedError extends Error {
-  constructor(message?: string) {
-    super(message);
-    console.log(`ExpectedError: ${message}`);
-  }
-}
+export default class ExpectedError extends Error {}
