97f9a3b4a824d69c66d3cc5911129aad45ba3795
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-08-20 20:55:11 -0700

    Debug

--- src/console/progressBarCli.ts
+++ src/console/progressBarCli.ts
@@ -190,19 +190,17 @@ export default class ProgressBarCLI extends ProgressBar {
   }
 
   async setName(name: string): Promise<void> {
-    if (this.payload.name === name) {
-      console.log(`not setting name: ${name}`);
-      return;
-    }
+    // if (this.payload.name === name) {
+    //   return;
+    // }
     this.payload.name = name;
     await this.render(true);
   }
 
   async setSymbol(symbol: string): Promise<void> {
-    if (this.payload.symbol === symbol) {
-      console.log(`not setting symbol: ${symbol}`);
-      return;
-    }
+    // if (this.payload.symbol === symbol) {
+    //   return;
+    // }
     this.payload.symbol = symbol;
     await this.render(true);
   }
