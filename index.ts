77901ecbf6409296f122fcb28a63e5e7fa8ba211
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2024-12-30 14:30:04 -0800

    Use top-level awaits

--- index.ts
+++ index.ts
@@ -18,68 +18,66 @@ import Options from './src/types/options.js';
 // Monkey-patch 'fs' to help prevent Windows EMFILE errors
 gracefulFs.gracefulify(realFs);
 
-(async (): Promise<void> => {
-  const logger = new Logger();
-  logger.printHeader();
+const logger = new Logger();
+logger.printHeader();
 
-  if (!semver.satisfies(process.version, Package.ENGINES_NODE)) {
-    logger.error(`${Package.NAME} requires a Node.js version of ${Package.ENGINES_NODE}`);
-    process.exit(1);
-  }
+if (!semver.satisfies(process.version, Package.ENGINES_NODE)) {
+  logger.error(`${Package.NAME} requires a Node.js version of ${Package.ENGINES_NODE}`);
+  process.exit(1);
+}
 
-  process.once('SIGINT', () => {
-    ProgressBarCLI.stop();
-    logger.newLine();
-    logger.notice(`Exiting ${Package.NAME} early`);
+process.once('SIGINT', () => {
+  ProgressBarCLI.stop();
+  logger.newLine();
+  logger.notice(`Exiting ${Package.NAME} early`);
+  process.exit(0);
+});
+
+// Parse CLI arguments
+let options: Options;
+try {
+  const argv = process.argv.slice(2);
+  options = new ArgumentsParser(logger).parse(argv);
+  logger.setLogLevel(options.getLogLevel());
+
+  const argvString = argv
+    .map((arg) => {
+      if (!arg.includes(' ')) {
+        return arg;
+      }
+      return `"${arg.replace(/"/g, '\\"')}"`;
+    })
+    .join(' ');
+  logger.trace(`Parsing CLI arguments: ${argvString}`);
+  logger.trace(`Parsed CLI options: ${options.toString()}`);
+
+  if (options.getHelp()) {
     process.exit(0);
-  });
+  }
+} catch (error) {
+  // Explicitly do not log the stack trace, for readability
+  logger.error(error);
+  logger.newLine();
+  process.exit(1);
+}
 
-  // Parse CLI arguments
-  let options: Options;
-  try {
-    const argv = process.argv.slice(2);
-    options = new ArgumentsParser(logger).parse(argv);
-    logger.setLogLevel(options.getLogLevel());
+// Start the main process
+try {
+  new EndOfLifeChecker(logger).check(process.version);
+  new UpdateChecker(logger).check();
 
-    const argvString = argv
-      .map((arg) => {
-        if (!arg.includes(' ')) {
-          return arg;
-        }
-        return `"${arg.replace(/"/g, '\\"')}"`;
-      })
-      .join(' ');
-    logger.trace(`Parsing CLI arguments: ${argvString}`);
-    logger.trace(`Parsed CLI options: ${options.toString()}`);
-
-    if (options.getHelp()) {
-      process.exit(0);
-    }
-  } catch (error) {
-    // Explicitly do not log the stack trace, for readability
+  await new Igir(options, logger).main();
+  ProgressBarCLI.stop();
+} catch (error) {
+  ProgressBarCLI.stop();
+  if (error instanceof ExpectedError) {
     logger.error(error);
-    logger.newLine();
-    process.exit(1);
+  } else if (error instanceof Error && error.stack) {
+    // Log the stack trace to help with bug reports
+    logger.error(error.stack);
+  } else {
+    logger.error(error);
   }
-
-  // Start the main process
-  try {
-    new EndOfLifeChecker(logger).check(process.version);
-    new UpdateChecker(logger).check();
-
-    await new Igir(options, logger).main();
-    ProgressBarCLI.stop();
-  } catch (error) {
-    ProgressBarCLI.stop();
-    if (error instanceof ExpectedError) {
-      logger.error(error);
-    } else if (error instanceof Error && error.stack) {
-      // Log the stack trace to help with bug reports
-      logger.error(error.stack);
-    } else {
-      logger.error(error);
-    }
-    logger.newLine();
-    process.exit(1);
-  }
-})();
+  logger.newLine();
+  process.exit(1);
+}
