eab2b570fcf3163c150cbaf77ea073705ecb5896
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 18:40:37 -0800

    More features

--- .github/actions/docker-run-action/README.md
+++ .github/actions/docker-run-action/README.md
@@ -3,4 +3,6 @@
 [addnab/docker-run-action@v3](https://github.com/addnab/docker-run-action), except it solves:
 
 - https://github.com/addnab/docker-run-action/issues/62 Docker image version that's too old
+- https://github.com/addnab/docker-run-action/issues/41 inability to use `$GITHUB_OUTPUT`
 - https://github.com/addnab/docker-run-action/issues/38 extremely naive newline replacement
+- https://github.com/addnab/docker-run-action/issues/26 inability to use `$GITHUB_ENV`
--- .github/actions/docker-run-action/entrypoint.sh
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -10,9 +10,13 @@ fi
 
 # shellcheck disable=SC2086
 exec docker run \
-  --volume "/var/run/docker.sock":"/var/run/docker.sock" \
+  --volume "/var/run/docker.sock:/var/run/docker.sock" \
+  --volume "${GITHUB_ENV}:${GITHUB_ENV}" \
+  --volume "${GITHUB_OUTPUT}:${GITHUB_OUTPUT}" \
+  --env GITHUB_ENV \
+  --env GITHUB_OUTPUT \
   --env INPUT_RUN \
   ${INPUT_OPTIONS} \
   "--entrypoint=${INPUT_SHELL}" \
   "${INPUT_IMAGE}" \
-  -c 'echo "${INPUT_RUN}" > /run_file && chmod +x /run_file && /run_file'
+  -c "echo \"\${INPUT_RUN}\" > run_file && chmod +x run_file && ${INPUT_SHELL} run_file"
