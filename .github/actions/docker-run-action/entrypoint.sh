b8741c98eed4b34fba06c84e8d3af928dff9994b
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 19:18:38 -0800

    https://yossarian.net/til/post/github-actions-can-call-docker-images-as-workflow-steps/

--- .github/actions/docker-run-action/entrypoint.sh
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -16,9 +16,9 @@ echo "${INPUT_RUN}" > run_file && chmod +x run_file
 # shellcheck disable=SC2086
 exec docker run \
   --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
-  --mount "type=bind,source=${GITHUB_ENV},target=${GITHUB_ENV}" \
-  --mount "type=bind,source=${GITHUB_OUTPUT},target=${GITHUB_OUTPUT}" \
-  --mount "type=bind,source=${GITHUB_STEP_SUMMARY},target=${GITHUB_STEP_SUMMARY}" \
+  --mount "type=bind,source=/home/runner/work/_temp/_github_home,target=/github/home" \
+  --mount "type=bind,source=/home/runner/work/_temp/_github_workflow,target=/github/workflow" \
+  --mount "type=bind,source=/home/runner/work/_temp/_runner_file_commands,target=/github/file_commands" \
   --mount "type=bind,source=$(pwd)/run_file,target=/run_file,readonly" \
   --env GITHUB_ENV \
   --env GITHUB_OUTPUT \
