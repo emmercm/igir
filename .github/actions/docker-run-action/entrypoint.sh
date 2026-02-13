2f98e9c6d9a046e5954bf7a8cda75331436aaccb
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 18:59:50 -0800

    Fix?

--- .github/actions/docker-run-action/entrypoint.sh
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -17,7 +17,7 @@ exec docker run \
   --volume "${GITHUB_ENV}:${GITHUB_ENV}" \
   --volume "${GITHUB_OUTPUT}:${GITHUB_OUTPUT}" \
   --volume "${GITHUB_STEP_SUMMARY}:${GITHUB_STEP_SUMMARY}" \
-  --volume "run_file:/run_file" \
+  --volume "$(pwd)/run_file:/run_file:ro" \
   --env GITHUB_ENV \
   --env GITHUB_OUTPUT \
   --env GITHUB_STEP_SUMMARY \
@@ -25,4 +25,4 @@ exec docker run \
   ${INPUT_OPTIONS} \
   "--entrypoint=${INPUT_SHELL}" \
   "${INPUT_IMAGE}" \
-  -c "ls -al \$GITHUB_STEP_SUMMARY; /run_file"
+  -c "echo \$GITHUB_STEP_SUMMARY; ls -al \$GITHUB_STEP_SUMMARY; /run_file"
