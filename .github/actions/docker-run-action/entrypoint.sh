c4bd2a858a7c062dab5d6f868c0a494bd90920cb
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 18:09:25 -0800

    Commit the file

---
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -n "${INPUT_USERNAME}" ]; then
+  echo "${INPUT_PASSWORD}" | docker login "${INPUT_REGISTRY}" -u "${INPUT_USERNAME}" --password-stdin
+fi
+
+if [ -n "${INPUT_DOCKER_NETWORK}" ]; then
+  INPUT_OPTIONS="${INPUT_OPTIONS} --network ${INPUT_DOCKER_NETWORK}"
+fi
+
+echo "${INPUT_RUN}" > run_file
+exec docker run --volume "/var/run/docker.sock":"/var/run/docker.sock" ${INPUT_OPTIONS} "--entrypoint=${INPUT_SHELL}" "${INPUT_IMAGE}" -c "run_file"
