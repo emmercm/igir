b82d62a32aa11f1de60d1c4d0938265948fc3aae
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 18:31:17 -0800

    Run file

--- .github/actions/docker-run-action/entrypoint.sh
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -9,4 +9,10 @@ if [ -n "${INPUT_DOCKER_NETWORK}" ]; then
 fi
 
 # shellcheck disable=SC2086
-exec docker run --volume "/var/run/docker.sock":"/var/run/docker.sock" ${INPUT_OPTIONS} "--entrypoint=${INPUT_SHELL}" "${INPUT_IMAGE}" -c 'echo $INPUT_RUN'
+exec docker run \
+  --volume "/var/run/docker.sock":"/var/run/docker.sock" \
+  --env INPUT_RUN \
+  ${INPUT_OPTIONS} \
+  "--entrypoint=${INPUT_SHELL}" \
+  "${INPUT_IMAGE}" \
+  -c 'echo "${INPUT_RUN}" > /run_file && chmod +x /run_file && /run_file'
