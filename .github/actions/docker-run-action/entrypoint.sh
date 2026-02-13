516d203fa4225dc3e8f514abbb488db38486046d
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-12 19:08:09 -0800

    Use mounts

--- .github/actions/docker-run-action/entrypoint.sh
+++ .github/actions/docker-run-action/entrypoint.sh
@@ -14,15 +14,14 @@ ls -al
 
 # shellcheck disable=SC2086
 exec docker run \
-  --volume "/var/run/docker.sock:/var/run/docker.sock" \
-  --volume "${GITHUB_ENV}:${GITHUB_ENV}" \
-  --volume "${GITHUB_OUTPUT}:${GITHUB_OUTPUT}" \
-  --volume "${GITHUB_STEP_SUMMARY}:${GITHUB_STEP_SUMMARY}" \
-  --volume "$(pwd)/run_file:/run_file:ro" \
+  --mount "type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock" \
+  --mount "type=bind,source=${GITHUB_ENV},target=${GITHUB_ENV}" \
+  --mount "type=bind,source=${GITHUB_OUTPUT},target=${GITHUB_OUTPUT}" \
+  --mount "type=bind,source=${GITHUB_STEP_SUMMARY},target=${GITHUB_STEP_SUMMARY}" \
+  --mount "type=bind,source=$(pwd)/run_file,target=/run_file,readonly" \
   --env GITHUB_ENV \
   --env GITHUB_OUTPUT \
   --env GITHUB_STEP_SUMMARY \
-  --env INPUT_RUN \
   ${INPUT_OPTIONS} \
   "--entrypoint=${INPUT_SHELL}" \
   "${INPUT_IMAGE}" \
