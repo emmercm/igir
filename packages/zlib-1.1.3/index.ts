7e9524a1c4606f1d8970a5b306c765b3c15149d7
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-02-19 18:34:30 -0800

    Stash

--- packages/zlib-1.1.3/index.ts
+++ packages/zlib-1.1.3/index.ts
@@ -51,33 +51,41 @@ export type ZlibCompressionLevelKey = keyof typeof ZlibCompressionLevel;
 export type ZlibCompressionLevelValue =
   (typeof ZlibCompressionLevel)[keyof typeof ZlibCompressionLevel];
 
+// TODO(cemmer): this will cause compilers like Bun to include every architecture's prebuild
+//  into every binary, but Parcel import attribute macros require all code to be bundled
+import darwinArm64 from './addon-zlib-1.1.3/prebuilds/darwin-arm64/node.node' with { type: 'file' };
+import darwinX64 from './addon-zlib-1.1.3/prebuilds/darwin-x64/node.node' with { type: 'file' };
+import linuxArm64 from './addon-zlib-1.1.3/prebuilds/linux-arm64/node.node' with { type: 'file' };
+import linuxX64 from './addon-zlib-1.1.3/prebuilds/linux-x64/node.node' with { type: 'file' };
+import win32Arm64 from './addon-zlib-1.1.3/prebuilds/win32-arm64/node.node' with { type: 'file' };
+import win32X64 from './addon-zlib-1.1.3/prebuilds/win32-x64/node.node' with { type: 'file' };
+
 const zlib = ((): ZlibBinding => {
-  // TODO(cemmer): this will cause compilers like Bun to include every architecture's prebuild
-  //  into every binary, but Parcel import attribute macros require all code to be bundled
   try {
+    console.log(`${os.platform()}-${os.arch()}`);
     switch (`${os.platform()}-${os.arch()}`) {
       case 'darwin-arm64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/darwin-arm64/node.node`) as ZlibBinding;
+        return require(darwinArm64) as ZlibBinding;
       }
       case 'darwin-x64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/darwin-x64/node.node`) as ZlibBinding;
+        return require(darwinX64) as ZlibBinding;
       }
       case 'linux-arm64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/linux-arm64/node.node`) as ZlibBinding;
+        return require(linuxArm64) as ZlibBinding;
       }
       case 'linux-x64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/linux-x64/node.node`) as ZlibBinding;
+        return require(linuxX64) as ZlibBinding;
       }
       case 'win32-arm64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/win32-arm64/node.node`) as ZlibBinding;
+        return require(win32Arm64) as ZlibBinding;
       }
       case 'win32-x64': {
-        return require(`./addon-zlib-1.1.3/prebuilds/win32-x64/node.node`) as ZlibBinding;
+        return require(win32X64) as ZlibBinding;
       }
     }
   } catch {
     /* ignored */
   }
-  return require(`./addon-zlib-1.1.3/build/Release/zlib.node`) as ZlibBinding;
+  return require('./addon-zlib-1.1.3/build/Release/zlib.node') as ZlibBinding;
 })();
 export default zlib;
--- packages/zstd-1.5.5/index.ts
+++ packages/zstd-1.5.5/index.ts
@@ -165,34 +165,41 @@ export class ZstdDecompressStream extends stream.Transform {
   }
 }
 
+// TODO(cemmer): this will cause compilers like Bun to include every architecture's prebuild
+//  into every binary, but Parcel import attribute macros require all code to be bundled
+import darwinArm64 from './addon-zstd-1.5.5/prebuilds/darwin-arm64/node.node' with { type: 'file' };
+import darwinX64 from './addon-zstd-1.5.5/prebuilds/darwin-x64/node.node' with { type: 'file' };
+import linuxArm64 from './addon-zstd-1.5.5/prebuilds/linux-arm64/node.node' with { type: 'file' };
+import linuxX64 from './addon-zstd-1.5.5/prebuilds/linux-x64/node.node' with { type: 'file' };
+import win32Arm64 from './addon-zstd-1.5.5/prebuilds/win32-arm64/node.node' with { type: 'file' };
+import win32X64 from './addon-zstd-1.5.5/prebuilds/win32-x64/node.node' with { type: 'file' };
+
 const zstd = ((): ZstdBinding => {
-  // TODO(cemmer): this will cause compilers like Bun to include every architecture's prebuild
-  //  into every binary, but Parcel import attribute macros require all code to be bundled
   try {
     switch (`${os.platform()}-${os.arch()}`) {
       case 'darwin-arm64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/darwin-arm64/node.node`) as ZstdBinding;
+        return require(darwinArm64) as ZstdBinding;
       }
       case 'darwin-x64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/darwin-x64/node.node`) as ZstdBinding;
+        return require(darwinX64) as ZstdBinding;
       }
       case 'linux-arm64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/linux-arm64/node.node`) as ZstdBinding;
+        return require(linuxArm64) as ZstdBinding;
       }
       case 'linux-x64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/linux-x64/node.node`) as ZstdBinding;
+        return require(linuxX64) as ZstdBinding;
       }
       case 'win32-arm64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/win32-arm64/node.node`) as ZstdBinding;
+        return require(win32Arm64) as ZstdBinding;
       }
       case 'win32-x64': {
-        return require(`./addon-zstd-1.5.5/prebuilds/win32-x64/node.node`) as ZstdBinding;
+        return require(win32X64) as ZstdBinding;
       }
     }
-  } catch {
-    /* ignored */
+  } catch (error) {
+    throw error;
   }
-  return require(`./addon-zstd-1.5.5/build/Release/binding.node`) as ZstdBinding;
+  return require('./addon-zstd-1.5.5/build/Release/binding.node') as ZstdBinding;
 })();
 export default {
   ...zstd,
