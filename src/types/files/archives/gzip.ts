c41c9ec35fdd82412d5375e6b155f95487b2b57a
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-01 23:56:30 -0800

    Fix?

--- src/types/files/archives/gzip.ts
+++ src/types/files/archives/gzip.ts
@@ -3,12 +3,13 @@ import stream from 'node:stream';
 import zlib from 'node:zlib';
 
 import IOFile from '../../../polyfill/ioFile.js';
+import IgirException from '../../exceptions/igirException.js';
 import Archive from './archive.js';
 import ArchiveEntry from './archiveEntry.js';
 import Tar from './tar.js';
 
 interface GzipHeaderFooter {
-  filename?: string;
+  fname?: string;
   crc32?: string;
   size?: number;
 }
@@ -39,8 +40,9 @@ export default class Gzip extends Archive {
       await ArchiveEntry.entryOf(
         {
           archive: this,
-          entryPath: '', // let CandidateExtensionCorrector sort it out
-          ...gzipHeaderFooter,
+          entryPath: gzipHeaderFooter.fname ?? '', // let CandidateExtensionCorrector sort it out
+          size: gzipHeaderFooter.size,
+          crc32: gzipHeaderFooter.crc32,
         },
         checksumBitmask,
       ),
@@ -53,7 +55,7 @@ export default class Gzip extends Archive {
       const header = await file.readAt(0, 10);
       if (header[0] !== 0x1f || header[1] !== 0x8b) {
         // Not a valid gzip file
-        return {};
+        throw new IgirException('missing gzip magic number');
       }
       const flags = header[3];
 
@@ -79,7 +81,7 @@ export default class Gzip extends Archive {
       const crc32 = footer.readUInt32LE().toString(16).toLowerCase();
       const size = footer.readUInt32LE(4);
 
-      return { filename, crc32, size };
+      return { fname: filename, crc32, size };
     } finally {
       await file.close();
     }
@@ -108,7 +110,8 @@ export default class Gzip extends Archive {
       },
     );
     try {
-      return await callback(readable);
+      const result = await callback(readable);
+      return result;
     } finally {
       readable.destroy();
     }
