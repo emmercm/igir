aa59ff4511dfbab02b0b6582593a3fd72641f197
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-03 15:59:28 -0800

    fs.createReadStream()

--- src/types/files/archives/sevenZip/sevenZip.ts
+++ src/types/files/archives/sevenZip/sevenZip.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
 import type { SevenZipEntry } from '7z-iterator';
@@ -50,64 +51,75 @@ export default class SevenZip extends Archive {
   }
 
   private async getArchiveEntriesNotCached(checksumBitmask: number): Promise<ArchiveEntry<this>[]> {
-    const iterator = new _7zIterator(this.getFilePath());
+    // Note: fs.createReadStream() is used for `graceful-fs`
+    const stream = fs.createReadStream(this.getFilePath());
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      for await (const _entry of iterator) {
-        // do nothing, just iterate everything so getStreamingOrder() works
+      const iterator = new _7zIterator(stream);
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for await (const _entry of iterator) {
+          // do nothing, just iterate everything so getStreamingOrder() works
+        }
+      } catch (error) {
+        if ((error as Error & { code: string }).code === 'CORRUPT_HEADER') {
+          // This will happen for valid archives with no files
+          return [];
+        }
+        throw error;
       }
-    } catch (error) {
-      if ((error as Error & { code: string }).code === 'CORRUPT_HEADER') {
-        // This will happen for valid archives with no files
-        return [];
-      }
-      throw error;
-    }
-    const entriesIn7z = iterator.getStreamingOrder();
+      const entriesIn7z = iterator.getStreamingOrder();
 
-    return await async.mapLimit(
-      entriesIn7z.filter((entry) => entry.type === 'file'),
-      //filesIn7z.filter((result) => !result.attr?.startsWith('D')),
-      Defaults.ARCHIVE_ENTRY_SCANNER_THREADS_PER_ARCHIVE,
-      async (entry: SevenZipEntry): Promise<ArchiveEntry<this>> => {
-        return await ArchiveEntry.entryOf(
-          {
-            archive: this,
-            entryPath: entry.path,
-            size: entry.size,
-            crc32: entry._crc?.toString(16).toLowerCase().padStart(8, '0'),
-            // If MD5, SHA1, or SHA256 is desired, this file will need to be extracted to calculate
-          },
-          checksumBitmask,
-        );
-      },
-    );
+      return await async.mapLimit(
+        entriesIn7z.filter((entry) => entry.type === 'file'),
+        Defaults.ARCHIVE_ENTRY_SCANNER_THREADS_PER_ARCHIVE,
+        async (entry: SevenZipEntry): Promise<ArchiveEntry<this>> => {
+          return await ArchiveEntry.entryOf(
+            {
+              archive: this,
+              entryPath: entry.path,
+              size: entry.size,
+              crc32: entry._crc?.toString(16).toLowerCase().padStart(8, '0'),
+              // If MD5, SHA1, or SHA256 is desired, this file will need to be extracted to calculate
+            },
+            checksumBitmask,
+          );
+        },
+      );
+    } finally {
+      stream.destroy();
+    }
   }
 
   async extractEntryToFile(entryPath: string, extractedFilePath: string): Promise<void> {
-    const iterator = new _7zIterator(this.getFilePath());
-    for await (const entry of iterator) {
-      if (entry.path !== entryPath) {
-        continue;
-      }
-
-      const extractDir = await FsPoly.mktemp(path.join(extractedFilePath));
-      try {
-        await entry.create(extractDir, {});
-        // 7z-iterator doesn't let you specify the exact file path
-        const extractedFiles = await FsPoly.walk(extractDir, WalkMode.FILES);
-        if (extractedFiles.length === 0) {
-          throw new IgirException(`failed to extract`);
+    // Note: fs.createReadStream() is used for `graceful-fs`
+    const stream = fs.createReadStream(this.getFilePath());
+    try {
+      const iterator = new _7zIterator(stream);
+      for await (const entry of iterator) {
+        if (entry.path !== entryPath) {
+          continue;
         }
-        await FsPoly.mv(extractedFiles[0], extractedFilePath);
-      } finally {
-        await FsPoly.rm(extractDir, { recursive: true, force: true });
+
+        const extractDir = await FsPoly.mktemp(path.join(extractedFilePath));
+        try {
+          await entry.create(extractDir, {});
+          // 7z-iterator doesn't let you specify the exact file path
+          const extractedFiles = await FsPoly.walk(extractDir, WalkMode.FILES);
+          if (extractedFiles.length === 0) {
+            throw new IgirException(`failed to extract`);
+          }
+          await FsPoly.mv(extractedFiles[0], extractedFilePath);
+        } finally {
+          await FsPoly.rm(extractDir, { recursive: true, force: true });
+        }
+
+        iterator.end();
+        return;
       }
 
-      iterator.end();
-      return;
+      throw new IgirException(`failed to find archive entry`);
+    } finally {
+      stream.destroy();
     }
-
-    throw new IgirException(`failed to find archive entry`);
   }
 }
