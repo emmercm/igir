32971b3c692e8839179ae67f84bbaee0eecf6523
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-07 22:00:55 -0800

    Fix installHold.ts

--- scripts/installHold.ts
+++ scripts/installHold.ts
@@ -102,7 +102,7 @@ const heldBackDependencies = Object.entries(packageJson)
     ];
   });
 
-if (heldBackDependencies.length > 0) {
+if (heldBackDependencies.reduce((sum, deps) => sum + deps.length, 0) > 0) {
   process.stdout.write(
     `${JSON.stringify(Object.fromEntries(heldBackDependencies), undefined, 2)}\n`,
   );
