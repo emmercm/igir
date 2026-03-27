cac28cc7983f5c4f257cb72f2e43d93c27fda4ee
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-26 18:44:19 -0700

    Fix: debug dolphin-tool test failures

--- src/types/files/archives/dolphin/dolphin.ts
+++ src/types/files/archives/dolphin/dolphin.ts
@@ -28,6 +28,7 @@ export default abstract class Dolphin extends Archive {
       inputFilename: this.getFilePath(),
       binaryPreference: DolphinToolBinaryPreference.PREFER_PATH_BINARY,
     });
+    console.log(this.getFilePath(), size, digests);
     if (checksumBitmask & ChecksumBitmask.MD5) {
       // dolphin-tool only returns CRC32 and SHA1 digests
       digests.md5 = (
