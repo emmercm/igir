39195de99ffd5a6f1d534bba820f05008b1e4e54
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-07-08 00:39:06 -0400

    CI: prebuild armv7 native addons (#2371)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs';
+
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -16,10 +18,18 @@ export default defineConfig({
     maxWorkers: '75%',
     testTimeout:
       60_000 *
-      // macOS is consistently the fastest, with p99 test time <10sec
+      // macOS Apple Silicon is fast
       (process.platform === 'darwin' && process.arch !== 'x64' ? 0.5 : 1) *
-      // Ubuntu ARM frequently times out
-      (process.platform === 'linux' && process.arch === 'arm64' ? 2 : 1),
+      // Ubuntu ARM is slow
+      (process.platform === 'linux' && process.arch.startsWith('arm') ? 2 : 1) *
+      // QEMU (via Docker) is slow
+      // https://github.com/sindresorhus/is-docker/blob/59379f14b6dda26a0167fce55d80bf546857f92d/index.js
+      (process.platform === 'linux' &&
+      (fs.existsSync('/.dockerenv') ||
+        fs.readFileSync('/proc/self/cgroup', 'utf8').includes('docker') ||
+        fs.readFileSync('/proc/self/mountinfo', 'utf8').includes('/docker/containers/'))
+        ? 2
+        : 1),
 
     // Only run the committed test files
     exclude: [...configDefaults.exclude, '.*/**', 'dist/**', 'packages/*/deps/**'],
