b31bbde46f0e57aa3a2c4aca9a921c2b80405bc7
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-08-11 21:19:38 -0400

    Fix

--- src/cache/cache.ts
+++ src/cache/cache.ts
@@ -95,7 +95,8 @@ export default class Cache<V> {
   async getOrCompute(
     key: string,
     runnable: (key: string) => V | Promise<V>,
-    shouldRecompute?: (value: V) => Promise<boolean>,
+    // eslint-disable-next-line unicorn/consistent-boolean-name
+    shouldRecompute?: (value: V) => boolean | Promise<boolean>,
   ): Promise<V> {
     const cached = this.getUnsafe(key);
     if (
@@ -170,7 +171,8 @@ export default class Cache<V> {
   async getOrComputeAnyKeys(
     keys: string[],
     runnable: () => V | Promise<V>,
-    shouldRecompute?: (value: V) => Promise<boolean>,
+    // eslint-disable-next-line unicorn/consistent-boolean-name
+    shouldRecompute?: (value: V) => boolean | Promise<boolean>,
   ): Promise<V | undefined> {
     if (keys.length === 0) {
       return undefined;
